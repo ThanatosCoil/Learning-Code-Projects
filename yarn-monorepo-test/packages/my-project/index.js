558e97f0373bc5884857e5a934149161a805ba56
ThanatosCoil <thanatosxaos@gmail.com>
2025-05-28 19:12:18 +0300

    Add yarn-monorepo-test

---
+++ yarn-monorepo-test/packages/my-project/index.js
@@ -0,0 +1,6 @@
+import { add, subtract, multiply, divide } from "@thanatos/mymath";
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
