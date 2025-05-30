27d113a64d8725e3ecd48648871b57464a400925
ThanatosCoil <thanatosxaos@gmail.com>
2025-05-30 05:07:46 +0300

    Add framer-motion-study

---
+++ framer-motion-study/src/variants.ts
@@ -0,0 +1,6 @@
+export const variants = {
+  /*  Имена могут быть любыми, потому что мы по сути создаем объект с ключами-свойствами */
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.5 },
+};
